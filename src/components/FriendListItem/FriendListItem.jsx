ea98e1b27a49938df43e85b7b4cf18c99dce21e4
Alyona <olenapav97@gmail.com>
2024-12-07 16:46:34 +0200

    аа

---
+++ src/components/FriendListItem/FriendListItem.jsx
@@ -0,0 +1,28 @@
+import FriendListItem from '../FriendListItem/FriendListItem';
+import PropTypes from 'prop-types';
+
+const FriendList = ({ friends }) => {
+  return (
+    <ul>
+      {friends.map(({ id, avatar, name, isOnline }) => (
+        <li key={id}>
+          <FriendListItem avatar={avatar} name={name} isOnline={isOnline} />
+        </li>
+      ))}
+    </ul>
+  );
+};
+
+FriendList.propTypes = {
+  friends: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.number.isRequired,
+      avatar: PropTypes.string.isRequired,
+      name: PropTypes.string.isRequired,
+      isOnline: PropTypes.bool.isRequired,
+    })
+  ).isRequired,
+};
+
+
+export default FriendList;
